97443b68423f4a49fcd67bc0e033477498a37d02
Travis Placek <tplacek@uark.edu>
2014-11-24 22:15:33 -0600

    Fixed list users

--- listusers.php
+++ listusers.php
@@ -1,14 +1,17 @@
 <?php
 require 'init.php';
 
-$sql = "SELECT username FROM users where AND activated='1' ORDER BY RAND() LIMIT 32";
+$sql = "SELECT username FROM users where activated='1' ORDER BY username";
 $query= $db_conx->query($sql);
 $userlist= "";
 if( $query->num_rows > 0 ) {
-	while ($row=mysqli_fetch_array($query, MYSQLI_ASSOC)) {
+	$rows=0;
+	while ( $rows < $query->num_rows ) {
+		$row=$query->fetch_array(MYSQLI_ASSOC);
 		$u= $row["username"];
-		$userlist .= '<a href="user.php?u=' .$u.'" title="'.$u.'"></a>';
-	} 
+		$userlist .= $rows+1 . " " . $u . "<br>";
+		$rows++;
+	}
 
 ?>
 
@@ -17,6 +20,7 @@ if( $query->num_rows > 0 ) {
 <h3> Random People Chosen from the Database: </h3>
 <br>
 <?php 
+	echo "Number of rows: " . $query->num_rows . "<br>";
 	echo $userlist;
 } else {
 	echo "NO USERS";
