095ed6678d181b2186ae5f98b590ed88bc760a3a
Travis Placek <tplacek@uark.edu>
2014-11-24 21:32:36 -0600

    Fixed erroneous linebreak

--- connect.php
+++ connect.php
@@ -2,8 +2,7 @@
 $db_conx = new mysqli("localhost", "root", "TeamFTeamF", "teamf");
 // Evaluate the connection
 if ($db_conx->connect_errno > 0) {
-   echo 'Unable to connect to database [' . $db_conx->connect_error . 
-']';
+   echo 'Unable to connect to database [' . $db_conx->connect_error . ']';
    die();
 }
 ?>
