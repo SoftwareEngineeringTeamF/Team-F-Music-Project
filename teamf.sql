bc5915b16f0d665442d602075aa257e31a51b305
Travis Placek <tplacek@uark.edu>
2014-11-24 18:58:15 -0600

    DB Version 1

---
+++ teamf.sql
@@ -0,0 +1,82 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14.7
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Nov 25, 2014 at 12:57 AM
+-- Server version: 5.6.21
+-- PHP Version: 5.4.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `teamf`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `members`
+--
+
+CREATE TABLE IF NOT EXISTS `members` (
+  `memberid` int(11) NOT NULL AUTO_INCREMENT,
+  `songid` int(11) NOT NULL,
+  `playlistid` int(11) NOT NULL,
+  PRIMARY KEY (`memberid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `playlists`
+--
+
+CREATE TABLE IF NOT EXISTS `playlists` (
+  `playlistid` int(11) NOT NULL AUTO_INCREMENT,
+  `ownerid` int(11) NOT NULL,
+  `public` tinyint(1) NOT NULL,
+  `Title` varchar(255) NOT NULL,
+  `Description` varchar(1023) NOT NULL,
+  PRIMARY KEY (`playlistid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `songs`
+--
+
+CREATE TABLE IF NOT EXISTS `songs` (
+  `songid` int(11) NOT NULL AUTO_INCREMENT,
+  `uploader` int(11) NOT NULL,
+  `Title` varchar(255) NOT NULL,
+  `Artist` varchar(255) NOT NULL,
+  PRIMARY KEY (`songid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `userid` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  PRIMARY KEY (`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
