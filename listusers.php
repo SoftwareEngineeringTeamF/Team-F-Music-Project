014dffa48aba4069c46472c9847250ebe259b432
Travis Placek <tplacek@uark.edu>
2014-11-24 22:23:26 -0600

    Fixed listusers

--- listusers.php
+++ listusers.php
@@ -1,23 +1,23 @@
 <?php
 require 'init.php';
 
-$sql = "SELECT username FROM users where activated='1' ORDER BY username";
+$sql = "SELECT * FROM users where activated='1' ORDER BY username";
 $query= $db_conx->query($sql);
-$userlist= "";
+$userlist= "<table><tr><th>userid</th><th>username</th><th>email</th><th>activated</th><th>hash_act</th><th>firstname</th><th>lastname</th><th>ip</th><th>signup</th><th>lastlogin</th></tr>";
 if( $query->num_rows > 0 ) {
 	$rows=0;
 	while ( $rows < $query->num_rows ) {
 		$row=$query->fetch_array(MYSQLI_ASSOC);
 		$u= $row["username"];
-		$userlist .= $rows+1 . " " . $u . "<br>";
+		$userlist .= "<tr><td>" . $row['userid'] . "</td><td>" . $row["username"] . "</td><td>" . $row["email"] . "</td><td>" . $row["activated"] . "</td><td>" . $row["hash_act"] . "</td><td>" . $row["firstname"] . "</td><td>" . $row["lastname"] . "</td><td>" . $row["ip"] . "</td><td>" . $row["signup"] . "</td><td>" . $row["lastlogin"] . "</td></tr>";
 		$rows++;
 	}
+	$userlist .= "</table>";
 
 ?>
 
 <body>
 <div id="middle page" style="margin-left: 200px;">
-<h3> Random People Chosen from the Database: </h3>
 <br>
 <?php 
 	echo "Number of rows: " . $query->num_rows . "<br>";
