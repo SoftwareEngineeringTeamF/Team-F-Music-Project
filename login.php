41645e424d6f37a7a813fe1d3bc9452d1d973c1e
Travis Placek <tplacek@uark.edu>
2014-11-24 19:47:36 -0600

    Fixed login page

--- login.php
+++ login.php
@@ -9,41 +9,13 @@ if (isset ($_SESSION['userId'])) {
 ?>
 <form name="form1" method="post" action="checklogin.php<?php 
 if(isset($_GET['r'])) echo "?r=" . $_GET['r'];?>">
-	<table width="300" border="0" align="center" cellpadding="0" 
-cellspacing="1" bgcolor="#CCCCCC">
 		<?php if(isset($_GET['r'])) echo "<tr><td><strong>Please 
 Login to Continue</strong></td></tr>";?>
-		<tr>
-		<td>
-		<table width="100%" border="0" cellpadding="3" 
-cellspacing="1" bgcolor="#FFFFFF">
-			<tr>
-				<td colspan="3"><strong>Member Login 
-</strong></td>
-			</tr>
-			<tr>
-				<td width="78">Username</td>
-				<td width="6">:</td>
-				<td width="294"><input name="myusername" 
-type="text" id="myusername"></td>
-			</tr>
-			<tr>
-				<td>Password</td>
-				<td>:</td>
-				<td><input name="mypassword" 
-type="password" id="mypassword"></td>
-			</tr>
-			<tr>
-				<td>&nbsp;</td>
-				<td>&nbsp;</td>
-				<td><input type="submit" name="Submit" 
-value="Login"></td>
-			</tr>
-		</table>
-		</td>
-		</tr>
-	</table>
-		</form>
+<strong>Member Login</strong><br>
+Username:<input name="myusername" type="text" id="myusername"><br>
+Password:<input name="mypassword" type="password" id="mypassword"><br>
+<input type="submit" name="Submit" value="Login">
+</form>
 
 <p align="center"> <a href="signup.php">Register new account?</a>   </p>
 
