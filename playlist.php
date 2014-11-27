02afe55f21f06c5b64db0e29e70ea15237a5eb3a
Travis Placek <tplacek@uark.edu>
2014-11-27 03:07:13 -0600

    Playlist construct and load with test

--- playlist.php
+++ playlist.php
@@ -2,23 +2,32 @@
 require "song.php";
 
 class Playlist {
-	protected $playlistid;
-	protected $ownerid;
-	protected $public;
-	protected $title;
-	protected $description;
-	protected $num_songs;
-	protected $songs = array();
+	private $playlistid;
+	private $ownerid;
+	private $public;
+	private $title;
+	private $description;
+	private $num_songs;
+	private $songs;
 	
-	private function __construct() {
+	public function __construct() {
+        $this->songs = array();
 	}
 	
 	public function load( $playlistid ) {
-		$this->$playlistid = $playlistid;
+		$this->playlistid = $playlistid;
+        echo "[PLAYLIST] Loading ID: " . $playlistid . "<BR>";
 		$this->refresh();
 	}
 	
 	public function create( $ownerid, $public, $title, $description ) {
+        $db_conx = new mysqli("localhost", "root", "TeamFTeamF", "teamf");
+		// Evaluate the connection
+		if ($db_conx->connect_errno > 0) {
+			echo 'Unable to connect to database [' . $db_conx->connect_error . ']';
+			die();
+		}
+		
 		$this->ownerid = $ownerid;
 		$this->public = $public;
 		$this->title = $title;
@@ -31,25 +40,52 @@ class Playlist {
 	}
 	
 	protected function refresh() {
-		$sql = "SELECT * from playlists WHERE playlistid='" . $this->playlistid . "'";
+        $db_conx = new mysqli("localhost", "root", "TeamFTeamF", "teamf");
+		// Evaluate the connection
+		if ($db_conx->connect_errno > 0) {
+			echo 'Unable to connect to database [' . $db_conx->connect_error . ']';
+			die();
+		}
+		echo "[PLAYLIST] Refreshing with ID: " . $this->playlistid . "<BR>";
+		$sql = "SELECT * from playlists WHERE playlistid='" . $this->playlistid . "' LIMIT 1";
 		$result = $db_conx->query($sql);
-		$row = $query->fetch_assoc();
-		$title = $row['title'];
-		$description = $row['description'];
-		$ownerid = $row['ownerid'];
-		$public = $row['public'];
+        echo "[PLAYLIST] Playlists found with ID " . $this->playlistid . ": " . $result->num_rows . "<BR>";
+		while( $row = $result->fetch_assoc() ) {
+            $this->title = $row['title'];
+            echo "[PLAYLIST] Read Title: " . $this->title . "<br>";
+            $this->description = $row['description'];
+            echo "[PLAYLIST] Read Description: " . $this->description . "<br>";
+            $this->ownerid = $row['ownerid'];
+            echo "[PLAYLIST] Read OwnerID: " . $this->ownerid . "<br>";
+            $this->public = $row['public'];
+            echo "[PLAYLIST] Read Public: " . $this->public . "<br>";
+        }
 		
 		// get array of song id's in order
-		$sql = "SELECT * from members WHERE playlistid='" . $this->playlistid . "' ORDER BY index";
+		$sql = "SELECT * from members WHERE playlistid='" . $this->playlistid . "' ORDER BY listindex";
 		$result = $db_conx->query($sql);
+        echo "[PLAYLIST] Loading " . $result->num_rows . " songs.<br>";
 		$this->num_songs = $result->num_rows;
-		while($row = $query->fetch_assoc()) {
-			$songs[$row['index']] = new Song($row['songid']);
+        $n = 0;
+		while($n < $this->num_songs) {
+            $row = $result->fetch_assoc();
+            echo "[PLAYLIST] Added songs[".$n."] ID:" . $row['songid'] . "<BR>";
+			//$songs[$row['listindex']] = new Song();
+            //$songs[$row['listindex']]->load($row['songid']);
+            $this->songs[$n] = $row['songid'];
+            $n++;
 		}
 		
 	}
 		
 	public function setTitle( $title ) {
+        $db_conx = new mysqli("localhost", "root", "TeamFTeamF", "teamf");
+		// Evaluate the connection
+		if ($db_conx->connect_errno > 0) {
+			echo 'Unable to connect to database [' . $db_conx->connect_error . ']';
+			die();
+		}
+		
 		$this->title = $title;
 		$sql = "UPDATE playlists SET title='" . $title . "' WHERE playlistid='" . $this->playlistid ."'";
 		$result = $db_conx->query($sql);
@@ -57,6 +93,13 @@ class Playlist {
 	}
 	
 	public function setDescription( $artist ) {
+        $db_conx = new mysqli("localhost", "root", "TeamFTeamF", "teamf");
+		// Evaluate the connection
+		if ($db_conx->connect_errno > 0) {
+			echo 'Unable to connect to database [' . $db_conx->connect_error . ']';
+			die();
+		}
+		
 		$this->description = $description;
 		$sql = "UPDATE playlists SET artist='" . $artist . "' WHERE playlistid='" . $this->playlistid ."'";
 		$result = $db_conx->query($sql);
@@ -64,42 +107,53 @@ class Playlist {
 	}
 	
 	public function setPublic( $public ) {
+        $db_conx = new mysqli("localhost", "root", "TeamFTeamF", "teamf");
+		// Evaluate the connection
+		if ($db_conx->connect_errno > 0) {
+			echo 'Unable to connect to database [' . $db_conx->connect_error . ']';
+			die();
+		}
+		
 		$this->public = $public;
 		$sql = "UPDATE playlists SET public='" . $artist . "' WHERE playlistid='" . $this->playlistid ."'";
 		$result = $db_conx->query($sql);
 		$this->refresh();
 	}
-	public function getSong( $index ) {
-		if(isset($song[$index]))
-			return $this->song[$index];
-		else
-			return false;
+	public function getSong( $listindex ) {
+        echo "[PLAYLIST] Getting Song ID: " . $listindex . "<br>";
+        //if(isset($song[$listindex]))
+        //    return $this->song[$listindex];
+        //else
+        //    return false;
+        $s = new Song();
+        $s->load($this->songs[$listindex]);
+        return $s;
 	}
 	
-	public function promoteSong( $index ) {
-		if( $index <= 0 || $index >= $this->num_songs) {
+	public function promoteSong( $listindex ) {
+		if( $listindex <= 0 || $listindex >= $this->num_songs) {
 			return false;
-		} else if(isset($song[$index])) {
-			$sql = "SELECT membersid, songid FROM members WHERE playlistid='" . $this->playlistid . "' AND index='" . $index . "'";
+		} else if(isset($song[$listindex])) {
+			$sql = "SELECT membersid, songid FROM members WHERE playlistid='" . $this->playlistid . "' AND listindex='" . $index . "'";
 			$result = $db_conx->query($sql);
 			$row = $result->fetch_assoc();
 			$i0 = $row['membersid'];
 			$s0 = $row['songid'];
-			$sql = "SELECT membersid, songid FROM members WHERE playlistid='" . $this->playlistid . "' AND index='" . $index-1 . "'";
+			$sql = "SELECT membersid, songid FROM members WHERE playlistid='" . $this->playlistid . "' AND listindex='" . $index-1 . "'";
 			$result = $db_conx->query($sql);
 			$result = $db_conx->query($sql);
 			$row = $result->fetch_assoc();
 			$i1 = $row['membersid'];
 			$s1 = $row['songid'];
 			
-			$sql = "UPDATE members SET index='" . $index-1 . "' WHERE membersid='" . $i0 ."'";
+			$sql = "UPDATE members SET listindex='" . $listindex-1 . "' WHERE membersid='" . $i0 ."'";
 			$result = $db_conx->query($sql);
-			$sql = "UPDATE members SET index='" . $index . "' WHERE membersid='" . $i1 ."'";
+			$sql = "UPDATE members SET listindex='" . $listindex . "' WHERE membersid='" . $i1 ."'";
 			$result = $db_conx->query($sql);
 			
-			$st = $songs[$index-1];
-			$songs[$index-1] = $songs[$index];
-			$songs[$index] = $st;
+			$st = $songs[$listindex-1];
+			$songs[$listindex-1] = $songs[$listindex];
+			$songs[$listindex] = $st;
 			
 			//or just completely recreate objects in array
 			//unset($song[$index]);
@@ -144,6 +198,10 @@ class Playlist {
 			return false;
 		}
 	}
+    
+    public function getNumSongs() {
+        return $this->num_songs;
+    }
 	
 	public function getDescription() {
 		return $this->description;
@@ -164,5 +222,9 @@ class Playlist {
 	public function getPublic() {
 		return $this->public;
 	}
+    
+    public function getSongs() {
+        return $this->songs;
+    }
 }
 ?>
---
+++ testplaylist.php
@@ -0,0 +1,38 @@
+<?php
+
+require "init.php";
+
+function echoplaylist(Playlist $p) {
+    echo "<table><tr><th>playlistid</th><th>ownerid</th><th>public</th><th>title</th><th>description</th></tr>";
+    echo "<tr><td>" . $p->getPlaylistId() . "</td><td>" . $p->getOwnerId() . "</td><td>" . $p->getPublic() . "</td><td>" . $p->getTitle() . "</td><td>" . $p->getDescription() . "</td></tr></table>";
+    
+    
+    if( $p->getNumSongs() > 0 ) {
+        echo "Number of songs in playlist: " . $p->getNumSongs() . "<BR>";
+        echo "<table><tr><th>index</th><th>artist</th><th>title</th></tr>";
+        $s = new Song();
+        $n = 1;
+        foreach( $p->getSongs() as $i ) {
+            
+            $s->load($i);
+            echo "<tr><td>" . ($n) . "</td><td>" . $s->getArtist() . "</td><td>" . $s->getTitle() . "</td></tr>";
+            $n++;
+        }
+        echo "</table>";
+    } else {
+        echo "EMPTY PLAYLIST";
+    }
+    echo "<BR>";
+}
+//test load
+$p0 = new Playlist();
+echo "<strong>Create Playlist Object and load 1 from db</strong><br>";
+$p0->load(1);
+echoplaylist($p0);
+if($p0->getPlaylistId() == 1) echo "PASS ID<BR>"; else echo "FAIL ID<BR>";
+if($p0->getOwnerId() == 1) echo "PASS OwnerID<BR>"; else echo "FAIL OwnerID<BR>";
+if($p0->getTitle() == "Title") echo "PASS Title<BR>"; else echo "FAIL Title<BR>";
+if($p0->getDescription() == "Description") echo "PASS Description<BR>"; else echo "FAIL Description<BR>"; echo "<HR>";
+
+include_once 'footer.php';
+?>
