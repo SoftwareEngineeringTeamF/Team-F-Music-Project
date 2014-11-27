9eda2ac520b141b647b4e16c7f58643230740874
Travis Placek <tplacek@uark.edu>
2014-11-26 18:56:57 -0600

    Added song class

---
+++ song.php
@@ -0,0 +1,50 @@
+<?php
+require "connect.php";
+
+class Song {
+	private $songid;
+	private $title;
+	private $artist;
+	private $owner;
+	
+	public function __construct( $songid ) {
+		$this->$songid = $songid;
+		$this->refresh();
+	}
+	
+	protected function refresh() {
+		$sql = "SELECT * from songs WHERE songid='" . $this->songid ."'";
+		$result = $db_conx->query($sql);
+		$row = $query->fetch_array(MYSQLI_ASSOC);
+		$title = $row['title'];
+		$artist = $row['artist'];
+		$owner = $row['owner'];
+	}
+	
+	public function setArtist( $artist ) {
+		$this->artist = $artist;
+		$sql = "UPDATE users SET artist='" . $artist . "' WHERE songid='" . $this->songid ."'";
+		$result = $db_conx->query($sql);
+		$this->refresh();
+	}
+	
+	public function setTitle( $title ) {
+		$this->title = $title;
+		$sql = "UPDATE songs SET title='" . $artist . "' WHERE songid='" . $this->songid ."'";
+		$result = $db_conx->query($sql);
+		$this->refresh();
+	}
+	
+	public function getArtist() {
+		return $this->artist;
+	}
+	
+	public function getTitle() {
+		return $this->title;
+	}
+	
+	public function getId() {
+		return $this->id;
+	}
+}
+?>
