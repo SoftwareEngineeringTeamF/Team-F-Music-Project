a75c223cf3c7bac2ade9198a0c53ee1b48734b72
Travis Placek <tplacek@uark.edu>
2014-12-08 21:36:05 -0600

    Added audio player to list playlists

--- listplaylists.php
+++ listplaylists.php
@@ -1,53 +1,47 @@
 <?php
 require 'init.php';
 require 'connect.php';
-
+$playlistlist = "";
 if(!isset($_GET[p])){
 
     $sql = "SELECT * FROM playlists ORDER BY playlistid";
     $query= $db_conx->query($sql);
-    $playlistlist= "<table><tr><th>playlistid</th><th>ownerid</th><th>public</th><th>title</th><th>description</tr>";
+    $playlistlist .= "<table><tr><th>playlistid</th><th>ownerid</th><th>public</th><th>title</th><th>description</tr>";
     if( $query->num_rows > 0 ) {
         while ( $row=$query->fetch_assoc() ) {
             $playlistlist .= "<tr><td>" . $row['playlistid'] . "</td><td>" . $row['ownerid'] . "</td><td>" . $row['public'] . "</td><td><a href=\"./listplaylists.php?p=" . $row['playlistid'] . "\">" . $row['title'] . "</td><td>" . $row['description'] . "</td></tr>";
         }
         $playlistlist .= "</table>";
+        $playlistlist .= "Number of rows: " . $query->num_rows . "<br>";
 
-?>
-
-<body>
-<div id="middle page" style="margin-left: 200px;">
-<br>
-<?php 
-        echo "Number of rows: " . $query->num_rows . "<br>";
-        echo $playlistlist;
     } else {
-        echo "NO PLAYLISTS";
+        $playlistlist .= "NO PLAYLISTS";
     }
 } else {
-    $sql = "SELECT songs.artist, songs.title, members.listindex FROM songs JOIN members on songs.songid = members.songid WHERE members.playlistid = '" . $_GET['p'] . "'  ORDER BY members.listindex";
+    $sql = "SELECT * FROM playlists WHERE playlistid= '" . $_GET['p'] . "' LIMIT 1";
     $query= $db_conx->query($sql);
-    $playlistlist= "<table><tr><th>index</th><th>artist</th><th>title</th></tr>";
+    $playlistlist .= "";
     if( $query->num_rows > 0 ) {
         while ( $row=$query->fetch_assoc() ) {
-            $playlistlist .= "<tr><td>" . ($row['listindex']+1) . "</td><td>" . $row['artist'] . "</td><td>" . $row['title'] . "</td></tr>";
+            $playlistlist .= '<div id="wrapper"><h1>' . $row['title'].  '</h1><audio preload></audio><ol>';
         }
-        $playlistlist .= "</table>";
+        
+        $sql = "SELECT songs.artist, songs.title, songs.songid, members.listindex FROM songs JOIN members on songs.songid = members.songid WHERE members.playlistid = '" . $_GET['p'] . "'  ORDER BY members.listindex";
+        $query= $db_conx->query($sql);
+        if( $query->num_rows > 0 ) {
+            while ( $row=$query->fetch_assoc() ) {
+                $playlistlist .= "<li>" . "<a href=\"#\" data-src=\"./songs/". $row['songid'] . ".mp3\">" . $row['artist'] . " - " . $row['title'] . "</a></li>";
+            }
+            $playlistlist .= "</ol></div>";
+            $playlistlist .= '<div id="shortcuts"><div><h1>Keyboard shortcuts:</h1><p><em>&rarr;</em> Next track</p><p><em>&larr;</em> Previous track</p><p><em>Space</em> Play/pause</p></div></div>';
 
-    
-    ?>
 
-<body>
-<div id="middle page" style="margin-left: 200px;">
-<br>
-<?php 
-        echo "Number of rows: " . $query->num_rows . "<br>";
-        echo $playlistlist;
-    } else {
-        echo "NO SONGS IN PLAYLIST";
+        } else {
+            $playlistlist .= "</ol></div>NO SONGS IN PLAYLIST";
+        }
+
     }
-
 }
-
+    echo $playlistlist;
 	include_once "footer.php";
 ?>
